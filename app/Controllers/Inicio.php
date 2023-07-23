c2be284237a298223bf75ff3a8325da51bac553a
Manuel Peralta <desarrollo7@conafor.gob.mx>
2023-07-23 12:28:00 -0600

    Actualización al menu

--- app/Controllers/Inicio.php
+++ app/Controllers/Inicio.php
@@ -4,10 +4,26 @@ namespace App\Controllers;
 
 class Inicio extends BaseController
 {
+    private function links($arr){
+        $html = '<li class="breadcrumb-item active"><a href="#">SIGSAB 2023</a></li>';
+        $end = end($arr);
+        foreach ($arr as $key => $value) {
+            if ($value === $end) {
+                $html .= '<li class="breadcrumb-item active"><a href="'.$value.'">'.$key.'</a></li>';
+            }
+            else{
+                $html .= '<li class="breadcrumb-item"><a href="'.$value.'">'.$key.'</a></li>';
+            }
+        }
+        return $html;
+    }
+
     public function index(){
         $data['titulo'] = 'Inicio';
         $data['descripcion'] = 'Descripción del menu inicio';
         $data['icono'] = '';
+        $data['m_inicio']= 'activo';
+        $data['links'] = $this->links(array('Inicio'=>base_url()));
         echo view('header',$data);
         echo view('inicio');
         echo view('footer');
--- app/Views/header.php
+++ app/Views/header.php
@@ -139,303 +139,41 @@
                     <li class="nav-search">
 						<input type="text" class="form-control" placeholder="Buscar en menu.." data-sidebar-search="true">
 					</li>
-                    <li class="nav-header">Navigation</li>
-					<li class="has-sub active">
+                    <li class="nav-header">Administración</li>
+                    <li class="has-sub">
 						<a href="javascript:;">
-							<b class="caret"></b>
-							<i class="ion-ios-pulse"></i>
-							<span>Dashboard</span>
+							<i class="fa fa-users bg-pink"></i>
+							<span>Usuarios</span>
 						</a>
-						<ul class="sub-menu">
-							<li><a href="<?=base_url()?>">Dashboard v1</a></li>
+					</li>
+					<li class="has-sub">
+						<a href="javascript:;">
+							<i class="fa fa-cloud-download-alt bg-info"></i>
+							<span>Importación Datos</span>
+						</a>
+					</li>
+                    
+                    <li class="nav-header">Menu</li>
+					<li class="has-sub <?=(isset($m_inicio))?$m_inicio:''?>">
+						<a href="javascript:;">
+							<i class="ion-ios-home bg-green"></i>
+							<span>Inicio</span>
+						</a>
+						<!-- <ul class="sub-menu">
+							<li><a href="<?=base_url()?>">INicio v1</a></li>
 							<li class="active"><a href="index_v2.html">Dashboard v2</a></li>
 							<li><a href="index_v3.html">Dashboard v3</a></li>
-						</ul>
+						</ul> -->
 					</li>
-					<li class="has-sub">
-						<a href="javascript:;">
-							<span class="badge pull-right">10</span>
-							<i class="ion-ios-mail"></i>
-							<span>Email</span>
-						</a>
-						<ul class="sub-menu">
-							<li><a href="email_inbox.html">Inbox</a></li>
-							<li><a href="email_compose.html">Compose</a></li>
-							<li><a href="email_detail.html">Detail</a></li>
-						</ul>
-					</li>
+                    
 					<li>
-						<a href="widget.html">
-							<i class="ion-ios-nutrition bg-blue"></i> 
-							<span>Widgets <span class="label label-theme">NEW</span></span> 
+						<a href="#">
+                            <i class="ion-ios-color-filter bg-indigo"></i>
+							<span>Seguimiento</span> 
 						</a>
-					</li>
-					<li class="has-sub">
-						<a href="javascript:;">
-							<b class="caret"></b>
-							<i class="ion-ios-color-filter bg-indigo"></i>
-							<span>UI Elements <span class="label label-theme">NEW</span></span> 
-						</a>
-						<ul class="sub-menu">
-							<li><a href="ui_general.html">General <i class="fa fa-paper-plane text-theme"></i></a></li>
-							<li><a href="ui_typography.html">Typography</a></li>
-							<li><a href="ui_tabs_accordions.html">Tabs & Accordions</a></li>
-							<li><a href="ui_unlimited_tabs.html">Unlimited Nav Tabs</a></li>
-							<li><a href="ui_modal_notification.html">Modal & Notification <i class="fa fa-paper-plane text-theme"></i></a></li>
-							<li><a href="ui_widget_boxes.html">Widget Boxes</a></li>
-							<li><a href="ui_media_object.html">Media Object</a></li>
-							<li><a href="ui_buttons.html">Buttons <i class="fa fa-paper-plane text-theme"></i></a></li>
-							<li><a href="ui_icons.html">Icons</a></li>
-							<li><a href="ui_simple_line_icons.html">Simple Line Icons</a></li>
-							<li><a href="ui_ionicons.html">Ionicons</a></li>
-							<li><a href="ui_tree.html">Tree View</a></li>
-							<li><a href="ui_language_bar_icon.html">Language Bar & Icon</a></li>
-							<li><a href="ui_social_buttons.html">Social Buttons</a></li>
-							<li><a href="ui_tour.html">Intro JS</a></li>
-						</ul>
-					</li>
-					<li>
-						<a href="bootstrap_4.html">
-							<div class="icon-img">
-								<img src="<?=base_url()?>assets/img/logo/logo-bs4.png" alt="" />
-							</div>
-							<span>Bootstrap 4 <span class="label label-theme">NEW</span></span> 
-						</a>
-					</li>
-					<li class="has-sub">
-						<a href="javascript:;">
-							<b class="caret"></b>
-							<i class="ion-ios-briefcase bg-gradient-purple"></i>
-							<span>Form Stuff <span class="label label-theme">NEW</span></span> 
-						</a>
-						<ul class="sub-menu">
-							<li><a href="form_elements.html">Form Elements <i class="fa fa-paper-plane text-theme"></i></a></li>
-							<li><a href="form_plugins.html">Form Plugins <i class="fa fa-paper-plane text-theme"></i></a></li>
-							<li><a href="form_slider_switcher.html">Form Slider + Switcher</a></li>
-							<li><a href="form_validation.html">Form Validation</a></li>
-							<li><a href="form_wizards.html">Wizards</a></li>
-							<li><a href="form_wizards_validation.html">Wizards + Validation</a></li>
-							<li><a href="form_wysiwyg.html">WYSIWYG</a></li>
-							<li><a href="form_editable.html">X-Editable</a></li>
-							<li><a href="form_multiple_upload.html">Multiple File Upload</a></li>
-							<li><a href="form_summernote.html">Summernote</a></li>
-							<li><a href="form_dropzone.html">Dropzone</a></li>
-						</ul>
-					</li>
-					<li class="has-sub">
-						<a href="javascript:;">
-							<b class="caret"></b>
-							<i class="ion-ios-grid bg-green"></i>
-							<span>Tables</span>
-						</a>
-						<ul class="sub-menu">
-							<li><a href="table_basic.html">Basic Tables</a></li>
-							<li class="has-sub">
-								<a href="javascript:;"><b class="caret"></b> Managed Tables</a>
-								<ul class="sub-menu">
-									<li><a href="table_manage.html">Default</a></li>
-									<li><a href="table_manage_autofill.html">Autofill</a></li>
-									<li><a href="table_manage_buttons.html">Buttons</a></li>
-									<li><a href="table_manage_colreorder.html">ColReorder</a></li>
-									<li><a href="table_manage_fixed_columns.html">Fixed Column</a></li>
-									<li><a href="table_manage_fixed_header.html">Fixed Header</a></li>
-									<li><a href="table_manage_keytable.html">KeyTable</a></li>
-									<li><a href="table_manage_responsive.html">Responsive</a></li>
-									<li><a href="table_manage_rowreorder.html">RowReorder</a></li>
-									<li><a href="table_manage_scroller.html">Scroller</a></li>
-									<li><a href="table_manage_select.html">Select</a></li>
-									<li><a href="table_manage_combine.html">Extension Combination</a></li>
-								</ul>
-							</li>
-						</ul>
-					</li>
-					<li class="has-sub">
-						<a href="javascript:;">
-							<b class="caret"></b>
-							<i class="ion-ios-infinite bg-gradient-aqua"></i>
-							<span>Front End</span>
-						</a>
-						<ul class="sub-menu">
-							<li><a href="../../../frontend/template/template_one_page_parallax/index.html" target="_blank">One Page Parallax</a></li>
-							<li><a href="../../../frontend/template/template_blog/index.html" target="_blank">Blog</a></li>
-							<li><a href="../../../frontend/template/template_forum/index.html" target="_blank">Forum</a></li>
-							<li><a href="../../../frontend/template/template_e_commerce/index.html" target="_blank">E-Commerce</a></li>
-						</ul>
-					</li>
-					<li class="has-sub">
-						<a href="javascript:;">
-							<b class="caret"></b>
-							<i class="ion-ios-archive bg-gradient-blue"></i>
-							<span>Email Template</span>
-						</a>
-						<ul class="sub-menu">
-							<li><a href="email_system.html">System Template</a></li>
-							<li><a href="email_newsletter.html">Newsletter Template</a></li>
-						</ul>
-					</li>
-					<li class="has-sub">
-						<a href="javascript:;">
-							<b class="caret"></b>
-							<i class="ion-ios-podium bg-gradient-orange"></i>
-							<span>Chart <span class="label label-theme">NEW</span></span>
-						</a>
-						<ul class="sub-menu">
-							<li><a href="chart-flot.html">Flot Chart</a></li>
-							<li><a href="chart-morris.html">Morris Chart</a></li>
-							<li><a href="chart-js.html">Chart JS</a></li>
-							<li><a href="chart-d3.html">d3 Chart</a></li>
-							<li><a href="chart-apex.html">Apex Chart <i class="fa fa-paper-plane text-theme"></i></a></li>
-						</ul>
-					</li>
-					<li>
-						<a href="calendar.html">
-							<i class="ion-ios-calendar bg-pink"></i> 
-							<span>Calendar</span>
-						</a>
-					</li>
-					<li class="has-sub">
-						<a href="javascript:;">
-							<b class="caret"></b>
-							<i class="ion-ios-map bg-pink"></i>
-							<span>Map</span>
-						</a>
-						<ul class="sub-menu">
-							<li><a href="map_vector.html">Vector Map</a></li>
-							<li><a href="map_google.html">Google Map</a></li>
-						</ul>
-					</li>
-					<li class="has-sub">
-						<a href="javascript:;">
-							<b class="caret"></b>
-							<i class="ion-ios-images"></i>
-							<span>Gallery</span>
-						</a>
-						<ul class="sub-menu">
-							<li><a href="gallery.html">Gallery v1</a></li>
-							<li><a href="gallery_v2.html">Gallery v2</a></li>
-						</ul>
-					</li>
-					<li class="has-sub">
-						<a href="javascript:;">
-							<b class="caret"></b>
-							<i class="ion-ios-cog"></i>
-							<span>Page Options <span class="label label-theme">NEW</span></span>
-						</a>
-						<ul class="sub-menu">
-							<li><a href="page_blank.html">Blank Page</a></li>
-							<li><a href="page_with_footer.html">Page with Footer</a></li>
-							<li><a href="page_without_sidebar.html">Page without Sidebar</a></li>
-							<li><a href="page_with_right_sidebar.html">Page with Right Sidebar</a></li>
-							<li><a href="page_with_minified_sidebar.html">Page with Minified Sidebar</a></li>
-							<li><a href="page_with_two_sidebar.html">Page with Two Sidebar</a></li>
-							<li><a href="page_with_line_icons.html">Page with Line Icons</a></li>
-							<li><a href="page_with_ionicons.html">Page with Ionicons</a></li>
-							<li><a href="page_full_height.html">Full Height Content</a></li>
-							<li><a href="page_with_wide_sidebar.html">Page with Wide Sidebar</a></li>
-							<li><a href="page_with_light_sidebar.html">Page with Light Sidebar</a></li>
-							<li><a href="page_with_mega_menu.html">Page with Mega Menu</a></li>
-							<li><a href="page_with_top_menu.html">Page with Top Menu</a></li>
-							<li><a href="page_with_boxed_layout.html">Page with Boxed Layout</a></li>
-							<li><a href="page_with_mixed_menu.html">Page with Mixed Menu</a></li>
-							<li><a href="page_boxed_layout_with_mixed_menu.html">Boxed Layout with Mixed Menu</a></li>
-							<li><a href="page_with_transparent_sidebar.html">Page with Transparent Sidebar</a></li>
-							<li><a href="page_with_search_sidebar.html">Page with Search Sidebar <i class="fa fa-paper-plane text-theme"></i></a></li>
-						</ul>
-					</li>
-					<li class="has-sub">
-						<a href="javascript:;">
-							<b class="caret"></b>
-							<i class="ion-ios-heart"></i>
-							<span>Extra <span class="label label-theme">NEW</span></span>
-						</a>
-						<ul class="sub-menu">
-							<li><a href="extra_timeline.html">Timeline</a></li>
-							<li><a href="extra_coming_soon.html">Coming Soon Page</a></li>
-							<li><a href="extra_search_results.html">Search Results</a></li>
-							<li><a href="extra_invoice.html">Invoice</a></li>
-							<li><a href="extra_404_error.html">404 Error Page</a></li>
-							<li><a href="extra_profile.html">Profile Page</a></li>
-							<li><a href="extra_scrum_board.html">Scrum Board <i class="fa fa-paper-plane text-theme"></i></a></li>
-							<li><a href="extra_cookie_acceptance_banner.html">Cookie Acceptance Banner <i class="fa fa-paper-plane text-theme"></i></a></li>
-						</ul>
-					</li>
-					<li class="has-sub">
-						<a href="javascript:;">
-							<b class="caret"></b>
-							<i class="ion-ios-lock"></i>
-							<span>Login & Register</span>
-						</a>
-						<ul class="sub-menu">
-							<li><a href="login.html">Login</a></li>
-							<li><a href="login_v2.html">Login v2</a></li>
-							<li><a href="login_v3.html">Login v3</a></li>
-							<li><a href="register_v3.html">Register v3</a></li>
-						</ul>
-					</li>
-					<li class="has-sub">
-						<a href="javascript:;">
-							<b class="caret"></b>
-							<i class="ion-ios-flower"></i>
-							<span>Version <span class="label label-theme">NEW</span></span>
-						</a>
-						<ul class="sub-menu">
-							<li><a href="../template_html/index_v3.html">HTML</a></li>
-							<li><a href="../template_ajax/">AJAX</a></li>
-							<li><a href="../template_angularjs/">ANGULAR JS</a></li>
-							<li><a href="../template_angularjs8/">ANGULAR JS 8</a></li>
-							<li><a href="../template_laravel/">LARAVEL</a></li>
-							<li><a href="../template_vuejs/">VUE JS</a></li>
-							<li><a href="../template_reactjs/">REACT JS</a></li>
-							<li><a href="../template_material/index_v3.html">MATERIAL DESIGN</a></li>
-							<li><a href="../template_apple/index_v3.html">APPLE DESIGN</a></li>
-							<li><a href="../template_transparent/index_v3.html">TRANSPARENT DESIGN <i class="fa fa-paper-plane text-theme"></i></a></li>
-							<li><a href="../template_facebook/index_v3.html">FACEBOOK DESIGN <i class="fa fa-paper-plane text-theme"></i></a></li>
-							<li><a href="../template_google/index_v3.html">GOOGLE DESIGN <i class="fa fa-paper-plane text-theme"></i></a></li>
-						</ul>
-					</li>
-					<li class="has-sub">
-						<a href="javascript:;">
-							<b class="caret"></b>
-							<i class="ion-ios-medkit"></i>
-							<span>Helper</span>
-						</a>
-						<ul class="sub-menu">
-							<li><a href="helper_css.html">Predefined CSS Classes</a></li>
-						</ul>
-					</li>
-					<li class="has-sub">
-						<a href="javascript:;">
-							<b class="caret"></b>
-							<i class="ion-ios-list"></i>
-							<span>Menu Level</span>
-						</a>
-						<ul class="sub-menu">
-							<li class="has-sub">
-								<a href="javascript:;">
-									<b class="caret"></b>
-									Menu 1.1
-								</a>
-								<ul class="sub-menu">
-									<li class="has-sub">
-										<a href="javascript:;">
-											<b class="caret"></b>
-											Menu 2.1
-										</a>
-										<ul class="sub-menu">
-											<li><a href="javascript:;">Menu 3.1</a></li>
-											<li><a href="javascript:;">Menu 3.2</a></li>
-										</ul>
-									</li>
-									<li><a href="javascript:;">Menu 2.2</a></li>
-									<li><a href="javascript:;">Menu 2.3</a></li>
-								</ul>
-							</li>
-							<li><a href="javascript:;">Menu 1.2</a></li>
-							<li><a href="javascript:;">Menu 1.3</a></li>
-						</ul>
 					</li>
 					<!-- begin sidebar minify button -->
-					<li><a href="javascript:;" class="sidebar-minify-btn" data-click="sidebar-minify"><i class="ion-ios-arrow-back"></i> <span>Collapse</span></a></li>
+					<li><a href="javascript:;" class="sidebar-minify-btn" data-click="sidebar-minify"><i class="ion-ios-arrow-back"></i> <span>Minimizar</span></a></li>
 					<!-- end sidebar minify button -->
 				</ul>
 				<!-- end sidebar nav -->
@@ -449,9 +187,7 @@
 		<div id="content" class="content">
 			<!-- begin breadcrumb -->
 			<ol class="breadcrumb float-xl-right">
-				<li class="breadcrumb-item"><a href="javascript:;">Home</a></li>
-				<li class="breadcrumb-item"><a href="javascript:;">Dashboard</a></li>
-				<li class="breadcrumb-item active">Dashboard v2</li>
+                <?=$links?>
 			</ol>
 			<!-- end breadcrumb -->
 			<!-- begin page-header -->
